89d49f8b097bfe7f5ef2d9e8df7554a687ee73a7
Patrick Terlisten <patrick@blazilla.de>
2018-01-17 08:07:07 +0100

    Modified link to website

--- Reclaim-ThinVMDK.ps1
+++ Reclaim-ThinVMDK.ps1
@@ -29,7 +29,7 @@
     International License (https://creativecommons.org/licenses/by-nc-sa/4.0/).
     
    .LINK
-    http://www.vcloudnine.de
+    https://www.vcloudnine.de
 #>
 
 ### Please change the content of the variables for $PathToSDelete, $VIServer
